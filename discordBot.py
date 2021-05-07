7d238ec29611e440ebf4102939c6ef8ae8916d9a
marcoarceo <marcoarceo@outlook.com>
2021-05-07 11:17:29 -0700

    Converted the program from running as a Client to a Bot for additional functionality and convenience. Created a on ready message letting me know the bot is running and a simple command example.

--- discordBot.py
+++ discordBot.py
@@ -1,7 +1,16 @@
 import discord
 import os
 from decouple import config
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-client.run(config('TOKEN'))
+@bot.event
+async def on_ready():
+    print("{0.user.name}".format(bot) + " has been activated")
+
+@bot.command(name='hello')
+async def SayHello(ctx):
+    await ctx.send("Hello {}".format(ctx.message.author.mention))
+
+bot.run(config('TOKEN'))
