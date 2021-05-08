80c7b29dfe2ac3fa815c409479113aff4129a9c6
marcoarceo <marcoarceo@outlook.com>
2021-05-07 17:55:25 -0700

    Added the ability for the bot to play music/audio in a discord server. This involves downloading the audio from youtube using ffmpeg and youtube_dl. Next steps involve adding a pause, stop, resume, and leave option for the bot.

--- discordBot.py
+++ discordBot.py
@@ -1,5 +1,8 @@
 import discord
 import os
+import random
+import youtube_dl
+
 from decouple import config
 from discord.ext import commands
 
@@ -12,5 +15,47 @@ async def on_ready():
 @bot.command(name='hello')
 async def SayHello(ctx):
     await ctx.send("Hello {}".format(ctx.message.author.mention))
+
+@bot.command(name='play')
+async def PlayMusic(ctx, url : str):
+    # Check if there is already a song being played or stored
+    currentSong = os.path.isfile("song.mp3")
+    try:
+        if currentSong:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    # Get the corresponding voice channel the message author is associated with
+    voiceChannel = ctx.message.author.voice.channel
+    if voiceChannel is None:
+        await ctx.send("Please be in a voice channel when using this command")
+        return
+        
+    # Connect the bot to the voice channel
+    await voiceChannel.connect()
+    # Get the bots voice client
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    # The youtube download settings that we need
+    audioSettings = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    # Download the audio as an mp3
+    with youtube_dl.YoutubeDL(audioSettings) as downloadedAudio:
+        downloadedAudio.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+
+    # Play the audio through the bot
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 bot.run(config('TOKEN'))
