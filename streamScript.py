aeee2a2002b2b482bfb15ade4a037f453d6c97a4
marcoarceo <marcoarceo@outlook.com>
2021-05-11 12:37:19 -0700

    Used Selenium to check if the user was either live or not, ran into an issue where some streamers had bug showing that they were live when they were not. Fixed this by creating a nested try/exception to make sure that both the live and offline elements were not present. Also created a schedule that willl execute the job every 10 minutes. This is in relation to issue #2 and issue #3.

--- streamScript.py
+++ streamScript.py
@@ -11,15 +11,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.edge.options import Options
 
+def CheckStatus(browser):
+    try: 
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'live-indicator-container tw-border-radius-large tw-inline')]")))
+        try: 
+            WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'channel-status-info channel-status-info--offline tw-border-radius-medium tw-inline-block')]")))
+            print("The user is offline")
+        except TimeoutException as ex:
+            print ("The user is live")
+    except TimeoutException as ex:
+        print("The user is offline")
+
+def UrlLoop(browser, urls):
+    for url in urls:
+        browser.get(url)
+        CheckStatus(browser)
 
 
 if __name__ == '__main__':
+    # Need to extract all of the urls from the database into a list
+    urls = ['https://www.twitch.tv/xqcow', 'https://www.twitch.tv/asunaweeb', 'https://www.twitch.tv/grimm', 'https://www.twitch.tv/timthetatman', 'https://www.twitch.tv/shroud']
+
     # Instantiate the webdriver with the executable location of MS Edge
     browser = webdriver.Edge(r"C:\Program Files\Python37\msedgedriver.exe")
 
     # Simply just open a new Edge browser and go to the url specified above
     browser.maximize_window()
-    browser.get(url)
 
-    schedule.every(10).minutes.do()
+    schedule.every(10).seconds.do(UrlLoop, browser, urls)
+
+    while 1:
+        schedule.run_pending()
 
